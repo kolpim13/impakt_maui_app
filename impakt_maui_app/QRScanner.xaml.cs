e4bcb74e5565d490ffe4bb45f4bbabd42fd0b02a
maksym.torhunakov <maksym.torhunakov@zf.com>
2025-06-10 11:40:19 +0200

    [Upd] QRScanner - add mode to modify pass type

--- impakt_maui_app/QRScanner.xaml.cs
+++ impakt_maui_app/QRScanner.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
+//using GameController;
 using ZXing.Net.Maui;
 using ZXing.Net.Maui.Controls;
 
@@ -14,10 +16,13 @@ namespace impakt_maui_app
         {
             None = 0,
             CheckIn,
+            UpdatePassType,
         }
 
         /* Data will be needed during existance of the page */
         private QRScanType scanType;
+
+        private string uptdate_pass_details_url;
 
         public QRScanner() 
         {
@@ -33,6 +38,8 @@ namespace impakt_maui_app
 
             /* Data initialzation */
             scanType = QRScanType.None;
+
+            uptdate_pass_details_url = string.Format("{0}/members/update/", Network.URL);
         }
 
         private async void OnBarcodesDetected(object? sender, BarcodeDetectionEventArgs e)
@@ -46,11 +53,31 @@ namespace impakt_maui_app
             {
                 if (!string.IsNullOrEmpty(value))
                 {
-                    await DisplayAlert("QR Code", value, "OK");
-                    await Navigation.PopAsync();
+                    // Call API depends on choosen parameters
+                    switch(scanType)
+                    {
+                        case QRScanType.UpdatePassType:
+                            PassType pass_type = (PassType)Enum.Parse(typeof(PassType), (string)PickerPassType.SelectedItem);
+                            BackendReq_UpdatePassDetails pass_details = new BackendReq_UpdatePassDetails
+                            {
+                                card_id = value,
+                                pass_type = (int)pass_type,
+                                entrances_left = Convert.ToInt16(PickerPassEntrances.SelectedItem),
+                                expiration_date = DateOnly.FromDateTime(DateTime.Today.AddDays(35)),
+                            };
 
-                    // Call the API with chosen previousely option
-                    await Task.Delay(1000); // Temporarly emulates databases response
+                            HttpClient client = new HttpClient();
+                            HttpResponseMessage response = await client.PostAsJsonAsync(uptdate_pass_details_url, pass_details);
+                            if (response.IsSuccessStatusCode)
+                            {
+
+                            }
+                            else
+                            {
+
+                            }
+                            break;
+                    }
 
                     // Restore possibility to scan after response is obtained
                     QRCodeReader.IsDetecting = true;
@@ -70,6 +97,9 @@ namespace impakt_maui_app
                     case "CheckEntrancesToday":
                         break;
                     case "UpdatePassData":
+                        scanType = QRScanType.UpdatePassType;
+                        PickerPassType.IsVisible = true;
+                        PickerPassEntrances.IsVisible = true;
                         break;
                 }
             }
@@ -84,12 +114,33 @@ namespace impakt_maui_app
             // Hide scanner and reveal buttons
             ButtonPanel.IsVisible = true;
             ScanningPannel.IsVisible = false;
+
+            // Restore values
+            switch (scanType)
+            {
+                case QRScanType.CheckIn:
+                    break;
+                case QRScanType.UpdatePassType:
+                    PickerPassType.IsVisible = false;
+                    PickerPassEntrances.IsVisible = false;
+                    break;
+            }
+            scanType = QRScanType.None;
         }
 
         private async void OnToMainPageClicked(object sender, EventArgs e)
         {
             await Shell.Current.GoToAsync("//MainPage");
             return;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            /* Select picker items */
+            PickerPassType.SelectedItem = (string)"No";
+            PickerPassEntrances.SelectedItem = (short)4;
         }
     }
 }
